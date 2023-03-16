99038bdfc0fae3d386450d0c789558382b132c7e
Uchenna winifred umegbolu <Uchennawinifredu@gmail.com>
2023-03-16 09:10:00 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -print a C programme using printf
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art.\n");
+	return (0);
+}
