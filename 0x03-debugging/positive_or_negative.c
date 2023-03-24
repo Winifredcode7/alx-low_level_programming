1a4e0b0957ac000524e23ac8fdb450efa7304627
Uchenna winifred umegbolu <Uchennawinifredu@gmail.com>
2023-03-23 19:23:35 -0700

    multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * positive_or_negative - checks for positive or negative number
+ *
+ * @i: number to be checked
+ *
+ * Return: always 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positve\n", i);
+}	
