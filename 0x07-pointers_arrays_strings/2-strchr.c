4d06cfe7c1135385424f8d1e3e8d479b530051c6
Uchenna winifred umegbolu <Uchennawinifredu@gmail.com>
2023-04-03 17:32:05 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ *@s: pointer to the first occurrence of the character
+ *@c: character that is pointed at
+ *Return: always 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	for (i = 0 ; s[i] >= '\0' ; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
