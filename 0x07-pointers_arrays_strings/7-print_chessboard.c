663e1f5f47d0ea1cddba10d8f24e7fe234d5df67
Uchenna winifred umegbolu <Uchennawinifredu@gmail.com>
2023-04-04 17:13:46 -0700

    Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer to pieces to print
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, n;
+
+	for (i = 0 ; i < 8 ; i++)
+	{
+		for (n = 0 ; n < 8 ; n++)
+		{
+			_putchar(a[i][n]);
+		}
+		_putchar('\n');
+	}
+}
