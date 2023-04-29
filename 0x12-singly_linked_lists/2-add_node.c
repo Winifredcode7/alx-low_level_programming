097b9095b23bd57eb04e9ae5eccd95e21c3f6196
uchenna winifred umegbolu <uchennawinifredu@gmail.com>
2023-04-28 17:50:27 -0700

    task 2. ADD NODE

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,32 +7,35 @@
  * @head: the double pointer to the list_t list
  * @str: the  string that would be  added in the node
  *
- * Return: the address of the new element, or NULL if it fails.
+ * Return:  NULL if it fails or returns address of new element
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new_node = malloc(sizeof(list_t));
 
-	if (head == NULL || str == NULL)
-	{
-		return (NULL);
-	}
-
-	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 
 	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		return (NULL);
-	}
+	new_node->next = NULL;
 
-	new_node->next = *head;
-	*head = new_node;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		list_t *current = *head;
+
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
+	}
 
 	return (new_node);
 }
