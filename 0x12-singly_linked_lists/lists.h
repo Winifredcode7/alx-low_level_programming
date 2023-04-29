7d530025b4e69de100322290b876ac8040d9c2b6
uchenna winifred umegbolu <uchennawinifredu@gmail.com>
2023-04-28 18:13:59 -0700

    task 00. PRINTS LIST

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,10 +3,10 @@
 
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * struct list_s - this is the singly linked list
+ * @str: string - (the malloc'ed string)
+ * @len: len for  length of the string
+ * @next: this  points to the next node
  *
  * Description: singly linked list node structure
  * for Holberton project
@@ -18,10 +18,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+size_t list_len(const list_t *h);
+size_t print_list(const list_t *h);
 
 #endif
